0e873313489d2ddd7ec9b6d4c31b19069980515e
Artem Kovalov <artemkov1990@gmail.com>
2024-10-23 20:49:03 +0300

    Implemented logic of getProvisioningFile() into service and controller

--- src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
+++ src/main/java/com/voxloud/provisioning/controller/ProvisioningController.java
@@ -1,11 +1,21 @@
 package com.voxloud.provisioning.controller;
 
+import com.voxloud.provisioning.service.ProvisioningService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping(value = "/api/v1")
+@RequiredArgsConstructor
 public class ProvisioningController {
 
-    // TODO Implement controller method
+    private final ProvisioningService provisioningService;
+
+    @GetMapping(value = "/provisioning/{mac}")
+    public String provisioning(@PathVariable String mac) {
+        return provisioningService.getProvisioningFile(mac);
+    }
 }
--- src/main/java/com/voxloud/provisioning/service/ProvisioningServiceImpl.java
+++ src/main/java/com/voxloud/provisioning/service/ProvisioningServiceImpl.java
@@ -1,12 +1,28 @@
 package com.voxloud.provisioning.service;
 
+import com.voxloud.provisioning.entity.Device;
+import com.voxloud.provisioning.exception.CustomException;
+import com.voxloud.provisioning.mapper.DeviceResponseMapper;
+import com.voxloud.provisioning.repository.DeviceRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
+@RequiredArgsConstructor
 public class ProvisioningServiceImpl implements ProvisioningService {
 
+    private final DeviceRepository deviceRepository;
+    private final DeviceResponseMapper deviceResponseMapper;
+
     public String getProvisioningFile(String macAddress) {
-        // TODO Implement provisioning
-        return null;
+        if (macAddress == null || macAddress.isEmpty()) {
+            throw new CustomException("Invalid mac address");
+        }
+
+        Device device = deviceRepository.findByMacAddress(macAddress.toLowerCase())
+                .orElseThrow(() -> new EntityNotFoundException(macAddress));
+        return deviceResponseMapper.mapToStringResponse(device);
     }
 }
